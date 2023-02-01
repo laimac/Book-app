cfc069cb9315a7177945f953056cf8c477cfe0e2
laimac <55thmouse@gmail.com>
2023-02-01 14:56:13 -0800

    styled login page

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 
 function Register() {
   return (
-    <div className="App">
-      <header className="App-header">
-123
-      </header>
+    <div className="auth">
+<h1>Register</h1>
+<form>
+<input required type="email" placeholder="e-mail address" />
+  <input required type="text" placeholder="username" />
+  <input required type="password" placeholder="password" />
+  <button>Register</button>
+  <span>Do you have an account?
+    <Link to="/login"> Login
+    </Link>
+  </span>
+</form>
     </div>
   );
 }
