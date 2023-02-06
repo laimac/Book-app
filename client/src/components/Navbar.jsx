a2f4766454c4a5342ff3a074729ef3b2f04c3fed
laimac <55thmouse@gmail.com>
2023-02-06 13:08:53 -0800

    Added and styled navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Logo from "../img/book.png";
+
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+            <img src={Logo} alt=""width="50"/>
+        </div>
+        <div className="links">
+            <Link className='link' to="/?cat=art">
+                <h6> Fantasy</h6>
+                </Link>
+                <Link className='link' to="/?cat=art">
+                <h6> Romance</h6>
+                </Link>
+                <Link className='link' to="/?cat=art">
+                <h6> Crime</h6>
+                </Link>
+                <Link className='link' to="/?cat=art">
+                <h6> Adventure</h6>
+                </Link>
+                <span>Laima</span>
+                <span>Logout</span>
+                <span className="write">
+                    <Link to="/write" className="link">Write</Link>
+                </span>
+        </div>
+      </div>
     </div>
   )
 }
